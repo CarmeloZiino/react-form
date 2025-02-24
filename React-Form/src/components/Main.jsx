ac7ba5f3ba9745cbe69274bcd593ae03aef5c386
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-02-24 14:44:25 +0100

    Add: Main.Jsx

--- React-Form/src/components/Main.jsx
+++ React-Form/src/components/Main.jsx
@@ -1,13 +1,44 @@
+import { useState } from 'react'
+import arrayCose from "../data/coseDaFare"
 
 const Main = () => {
+
+   const [ titleForm , setTitleForm] = useState("Mettere a mollo i legumi")
+   const handleChange = (e) =>{
+     console.log ( e )
+     console.log ( e.target)
+     setTitleForm ( e.target.value)
+  }
+
+// array iniziale reattivo
+  const [ task , setTasks] = useState ( arrayCose )
+
+  //Nuovo task da aggiungere
+  const [ newTask , setNewTask ] = useState ('')
+
     return (
       
       <main>
-          
-          
+          <div>
+            <ul>
+            {
+              arrayCose.map((element) => {
+                return(
+                <li>
+                  {element.titolo}
+                </li>)
+              })
+            }
+            </ul>
+          </div>
+          <input 
+      type="text"
+      value={titleForm}
+      onChange={ handleChange }
+      /> 
 
       </main>
   
     );
   };
-  export default Main
+  export default Main;
---
+++ React-Form/src/data/coseDaFare.js
@@ -0,0 +1,12 @@
+const arrayCose = [
+    { id: 1, titolo: "Come imparare React velocemente" },
+    { id: 2, titolo: "I migliori framework JavaScript nel 2025" },
+    { id: 3, titolo: "Guida a HTML e CSS per principianti" },
+    { id: 4, titolo: "10 trucchi per migliorare la logica di programmazione" },
+    { id: 5, titolo: "JavaScript: Funzioni e Callback spiegati semplicemente" },
+    { id: 6, titolo: "Introduzione a TypeScript per sviluppatori JS" },
+    { id: 7, titolo: "Come strutturare un progetto React professionale" },
+    { id: 8, titolo: "Le migliori risorse per diventare un programmatore web" }
+];
+
+export default arrayCose 
